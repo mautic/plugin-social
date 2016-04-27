34ef9569f0e7073c1be74966def3bc7117c4dab1
mqueme <mqueme@gmail.com>
2016-04-27 11:29:47 +0100

    changes to social login, to display social icons, and to have all social media enabled login buttons together

---
+++ EventListener/FormSubscriber.php
@@ -0,0 +1,45 @@
+<?php
+/**
+ * @package     Mautic
+ * @copyright   2014 Mautic Contributors. All rights reserved.
+ * @author      Mautic
+ * @link        http://mautic.org
+ * @license     GNU/GPLv3 http://www.gnu.org/licenses/gpl-3.0.html
+ */
+
+namespace MauticPlugin\MauticSocialBundle\EventListener;
+
+use Mautic\CoreBundle\EventListener\CommonSubscriber;
+use Mautic\FormBundle\Event\FormBuilderEvent;
+use Mautic\FormBundle\FormEvents;
+
+/**
+ * Class FormSubscriber
+ */
+class FormSubscriber extends CommonSubscriber
+{
+
+    /**
+     * {@inheritdoc}
+     */
+    static public function getSubscribedEvents()
+    {
+        return array(
+            FormEvents::FORM_ON_BUILD  => array('onFormBuild', 0)
+        );
+    }
+
+    /**
+     * @param FormBuilderEvent $event
+     */
+    public function onFormBuild(FormBuilderEvent $event)
+    {
+        $action = array(
+            'label'    => 'mautic.plugin.actions.socialLogin',
+            'formType' => 'sociallogin',
+            'template' => 'MauticSocialBundle:Integration:login.html.php',
+        );
+
+        $event->addFormField('plugin.loginSocial', $action);
+    }
+}
